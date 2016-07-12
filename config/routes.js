a26975e92df5a63e0b591c93e46605bc937b53fa
Wayne Patterson <suprsidr@gmail.com>
2016-07-12 05:43:10 -0500

    routes for stuman example

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Home from '../components/Home';
-import Products from '../components/Products';
 import Search from '../components/Search';
 import Main from '../components/Main';
-import ProdPage from '../components/ProdPage';
+import Student from '../components/Student';
+import Students from '../components/Students';
 import {NotFound} from '../components/NotFound';
 import {Route, IndexRoute} from 'react-router';
 
 export default (
   <Route path="/" component={Main}>
-    <Route path="/students" component={Studentss} />
+    <Route path="/students/:letter" component={Students} />
     <Route path="/student/:id" component={Student} />
     <Route path="/search/:searchTerm" component={Search} />
 	  <Route path="*" component={NotFound}/>
