391570db2e7149d68a9b0bad4dfc9803d42cc376
suprsidr <suprsidr@gmail.com>
2016-07-13 17:32:21 -0500

    matching methods for our endpoints

--- config/utils.js
+++ config/utils.js
@@ -35,6 +35,58 @@ export const fetchData = (
 };
 
 /**
+ * create new data on the server
+ * @param {Object} query eg:
+ * {
+      admin: 'Jim Smith',
+      student: {
+          "gender": "female",
+          "name": {
+              "title": "mrs",
+              "first": "abby",
+              "last": "baby"
+          },
+          "location": {
+              "street": "8229 plum st",
+              "city": "salinas",
+              "state": "wyoming",
+              "postcode": 94983
+          },
+          "email": "catherine.bailey@example.com",
+          "registered": 1323036434,
+          "dob": 368692954,
+          "phone": "(955)-735-0917",
+          "cell": "(974)-694-2005",
+          "picture": {
+              "large": "/women/89.jpg",
+              "medium": "/med/women/89.jpg",
+              "thumbnail": "/thumb/women/89.jpg"
+          },
+          "gpa": "3.0",
+          "major": "Multi/interdisciplinary Studies"
+      }
+    }
+ * @param {Function} cb
+ */
+export const insertRemoteData = (
+  query = {},
+  cb) => {
+  console.log('inserting data on the server');
+  const q = JSON.stringify(query);
+  request
+    .post(`${FETCH_URL}/insert/${q}`)
+    .end((err, res) => {
+      console.log('error: ' + err, 'body: ' + JSON.stringify(res.body));
+      if (err) {
+        cb(err);
+      } else {
+        const data = res.body.result.nModified === 1 ? res.body.student : null;
+        cb(null, data);
+      }
+    });
+};
+
+/**
  * update data on the server
  * @param {Object} query
  * @param {Function} cb
@@ -47,11 +99,32 @@ export const updateRemoteData = (
   request
     .post(`${FETCH_URL}/update/${q}`)
     .end((err, res) => {
-      console.log('error: ' + err, 'body: ' + JSON.stringify(res.body));
       if (err) {
         cb(err);
       } else {
         const data = res.body.result.nModified === 1 ? res.body.student : null;
+        cb(null, data);
+      }
+    });
+};
+
+/**
+ * delete item from data on the server
+ * @param {Object} query {"sid": "942c2bf7-60f1-480b-b471-1f9b71f08e05"}
+ * @param {Function} cb
+ */
+export const deleteRemoteData = (
+  query = {},
+  cb) => {
+  console.log('deleting data on the server');
+  const q = JSON.stringify(query);
+  request
+    .post(`${FETCH_URL}/delete/${q}`)
+    .end((err, res) => {
+      if (err) {
+        cb(err);
+      } else {
+        const data = res.body.result.ok === 1;
         cb(null, data);
       }
     });
@@ -63,3 +136,16 @@ export const upsert = (db, data, cb) => {
     cb && cb();
 	});
 }
+
+export const updateLocalStorage = (db) => {
+  db.students.find({}, {
+    limit: 0,
+    sort: {},
+    fields: {}
+  }).fetch((data) => {
+    localStorage.setItem('studentData', JSON.stringify({
+      timeStamp: Math.round(new Date().getTime() / 1000),
+      students: data
+    }));
+  });
+};
