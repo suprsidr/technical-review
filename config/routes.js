61a60ae23930ddc3cdcb2a8da902286471583825
suprsidr <suprsidr@gmail.com>
2016-07-14 04:16:42 -0500

    include / in option

--- config/routes.js
+++ config/routes.js
@@ -10,10 +10,10 @@ import {Route, IndexRoute} from 'react-router';
 
 export default (
   <Route path="/" component={Main}>
-    <Route path="/students/(:letter)" component={Students} />
+    <Route path="/students(/:letter)" component={Students} />
     <Route path="/student/:id" component={Student} />
     <Route path="/new" component={NewStudent} />
-    <Route path="/search/(:searchTerm)" component={Search} />
+    <Route path="/search(/:searchTerm)" component={Search} />
 	  <Route path="*" component={NotFound}/>
     <IndexRoute component={Home} />
   </Route>
