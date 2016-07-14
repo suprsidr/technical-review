af0790057cf487f2d7d48e904172686248c1158e
suprsidr <suprsidr@gmail.com>
2016-07-14 03:54:13 -0500

    home content Yay!

--- components/Home.js
+++ components/Home.js
@@ -4,8 +4,12 @@ import {Link} from 'react-router';
 export default class Home extends Component {
   render() {
     return (
-      <div>
-        <p>Stuff</p>
+      <div className="row">
+        <div className="small-12 columns text-center">
+          <h2>Welcome to StuMan</h2>
+          <h5>The Student Manager App</h5>
+          <p><Link to="/students/" activeStyle={{ color: '#00d8ff' }}><img src="/img/home.png" alt="Students"/></Link></p>
+        </div>
       </div>
     )
   }
